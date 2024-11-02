7323fd04421e82761aca3bf34d508cf118c5821f
leenaAhmed <leena3792@gmail.com>
2024-11-02 15:03:28 +0200

    add composed unction and limat api calls

--- closure.js
+++ closure.js
@@ -175,3 +175,70 @@ function expensiveOperation() {
 const executeOnce = once(expensiveOperation);
 console.log(executeOnce()); 
 console.log(executeOnce()); // Result
+
+
+//  Limit api calls
+
+/*
+    Create a function called limitCalls that takes a function and a maximum number of calls as arguments. 
+   The function should return a closure that only invokes the original function up to the specified maximum number of times.
+*/
+
+function limitCalls(fn, maximum) {
+    let count = 0;
+    return function (...args) {
+        if (count < maximum) {
+            ++count
+            return fn.apply(this, args);
+        }
+        else if (count <= maximum) {
+             console.log("Maximum number of calls reached!")
+        }
+    }
+}
+function logMessage(message) {
+    console.log(message);
+}
+  
+  const limitedLogMessage = limitCalls(logMessage, 3);
+  
+  limitedLogMessage('Hello'); // Output: Hello
+  limitedLogMessage('World'); // Output: World
+  limitedLogMessage('OpenAI'); // Output: OpenAI
+limitedLogMessage('Extra call'); // Output: Maximum number of calls reached!
+  
+
+
+/*
+ Write a function called compose that accepts multiple functions as arguments and returns a new function.
+ The new function should apply the input functions in reverse order, 
+ passing the result of each function call as an argument to the next.
+ 
+ */
+
+ function compose(...fun) {
+     return function (number) { 
+         // acc = 10 , func = double  =>  acc = 20
+         // acc = 20 , func = square => acc = 400
+         // acc = 400 , func = increment => acc = 401
+         return fun.reduceRight((acc, func) => {
+             console.log(func(acc))
+            return func(acc)
+         }, number);
+    }
+ }
+
+ function double(x) {
+    return x * 2; //1: 20 
+  }
+  function square(x) {
+    return x * x; // 2: 400
+  }  
+  
+  function increment(x) {
+    return x + 1; //3:  401
+  }
+  
+const composedFunction = compose(increment, square, double);
+const result = composedFunction(10);  
+console.log(result);
